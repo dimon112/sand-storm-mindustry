71a6ef9e63690974555bb31e98d6eaad4bde0eb2
dimon112 <92992092+dimon112@users.noreply.github.com>
2026-08-25 16:08:27 +0300

    fix

--- scripts/units/DelayedBuilderAI.js
+++ scripts/units/DelayedBuilderAI.js
@@ -1,18 +1,21 @@
 const DelayedBuilderAI = () => {
     return extend(BuilderAI, {
-        rebuildDelay: 1800, // 30 seconds (60 ticks * 30)
+        rebuildDelay: 1800, // 30 seconds bleh
         cooldown: 0,
         lastPlansCount: 0,
 
         updateUnit() {
-            let currentPlans = this.unit.team.plans.size;
+            let teamData = this.unit.team.data();
+            let currentPlans = teamData.plans.size;
 
             if (currentPlans > this.lastPlansCount) {
                 this.cooldown = this.rebuildDelay;
             }
             this.lastPlansCount = currentPlans;
+            
             if (this.cooldown > 0) {
                 this.cooldown -= Time.delta;
+                
                 this.unit.clearBuilding();
                 
                 let core = this.unit.closestCore();
@@ -20,13 +23,13 @@ const DelayedBuilderAI = () => {
                     this.moveTo(core, 120);
                 }
                 
-                return;
+                return; 
             }
+            
             this.super$updateUnit();
         }
     });
 };
 
 const sandDrone = extend(UnitType, "sand-drone", {});
-
 sandDrone.controller = prov(() => DelayedBuilderAI());

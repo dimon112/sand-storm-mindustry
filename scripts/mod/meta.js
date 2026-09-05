9155c294b5692a5404559b6d038c190d201ccad7
Oscillatorium <oskaradamgold1@gmail.com>
2026-09-05 12:24:27 +0500

    Create mod/meta.js

---
+++ scripts/mod/meta.js
@@ -0,0 +1,7 @@
+const mod = Vars.mods.locateMod("sand-stormv2").meta;
+
+// Загрузка метаданных из склада тапков
+mod.displayName = Core.bundle.get("mod.sand-stormv2.name");
+mod.author = Core.bundle.get("mod.sand-stormv2.author");
+mod.description = Core.bundle.get("mod.sand-stormv2.description");
+mod.subtitle = Core.bundle.get("mod.sand-stormv2.subtitle");
